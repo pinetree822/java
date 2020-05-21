ff52efbcee86e35543350f9b1530f87edb8ef56d
pinetree822 <pinetree822@nate.com>
2020-05-21 17:42:22 +0900

    git init

---
+++ workJava/.metadata/.plugins/org.eclipse.core.resources/.history/a2/f0684203349b001a1c39b99f607754b8
@@ -0,0 +1,10 @@
+package net.tis.day21;
+
+public class First {
+
+	public static void main(String[] args) {
+		// ÀÌ¸§ : ±èÅÂÈÆ
+		System.out.println("Å×½ºÆ®1");
+}
+
+}//class END
---
+++ workJava/Day0521/src/net/tis/day21/Hello.java
@@ -0,0 +1,24 @@
+package net.tis.day21;
+
+public class Hello {
+
+	public static int sum(int n, int m) {
+		return n+m;
+	}
+	public static void main(String[] args) {
+		
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i, 10);
+		a = '?';
+		
+		
+		// Test.java 출력연습
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
+	}
+
+}//class End
