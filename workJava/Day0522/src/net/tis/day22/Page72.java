56b8d05b2d3d55a23c32bf986ebf5beb6dd93b94
pinetree822 <pinetree822@nate.com>
2020-05-22 11:30:40 +0900

    git init

---
+++ workJava/Day0522/src/net/tis/day22/Page72.java
@@ -0,0 +1,16 @@
+package net.tis.day22;
+
+public class Page72 {
+
+	public static void main(String[] args) {
+		// Page71.java
+//		System.out.println();
+
+		String book = "저자 \"마크트웨인\"'s 톰소여모험";
+		System.out.println(book);
+		
+
+	}
+	
+
+}
---
+++ workJava/Day0522/src/net/tis/day22/package-info.java
@@ -0,0 +1,3 @@
+package net.tis.day22;
+
+
