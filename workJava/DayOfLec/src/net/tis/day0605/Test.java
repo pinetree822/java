865e14a1ef3528094cded11df4f6a805c1185daa
pinetree822 <pinetree822@nate.com>
2020-06-05 18:30:09 +0900

    git init

---
+++ workJava/DayOfLec/src/net/tis/day0605/Test.java
@@ -0,0 +1,27 @@
+package net.tis.day0605;
+
+
+
+
+public class Test {
+
+	public Test() {
+		
+	}// Consumer()
+	
+	public static void main(String[] args) {
+		
+		int data = 120;
+		if (data>100) {
+			System.out.println("Test클래스 수박");
+			System.out.println("Test클래스 파란색");
+			return;
+		} else {
+			System.out.println("Test클래스 하늘색");
+			System.out.println("Test클래스 노란색");
+			return;
+		}
+//		System.out.println("Test클래스 - 영수증 10:52");
+	}// main()
+	
+}// class Consumer
